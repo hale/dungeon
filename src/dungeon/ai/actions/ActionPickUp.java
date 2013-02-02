3421b912cbac2cc2935b62f00a50d9dd5917400e
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-02-02 20:56:54 +0000

    formatting

--- src/dungeon/ai/actions/ActionPickUp.java
+++ src/dungeon/ai/actions/ActionPickUp.java
@@ -11,28 +11,28 @@ import dungeon.model.items.treasure.Treasure;
  */
 public class ActionPickUp
 {
-	/**
-	 * Make the given mob pick up nearby treasure if possible
-	 * 
-	 * @param mob The mob
-	 * @param game The current game object
-	 * @return Returns true if the mob picked up treasure; false otherwise
-	 */
-	public static boolean performAction(Mob mob, Game game)
-	{
-		// Am I beside something I want to pick up?
-		Vector<Treasure> nearby_items = mob.nearbyTreasure(game);
-		for (Treasure treasure: nearby_items)
-		{
-			// Can I carry it?
-			double weight = treasure.getWeight() + mob.getEncumbrance();
-			if (weight > mob.getStrength())
-				continue;
-			
-			// Pick it up
-			return mob.pickUp(game, treasure);
-		}
-		
-		return false;
-	}
+  /**
+   * Make the given mob pick up nearby treasure if possible
+   *
+   * @param mob The mob
+   * @param game The current game object
+   * @return Returns true if the mob picked up treasure; false otherwise
+   */
+  public static boolean performAction(Mob mob, Game game)
+  {
+    // Am I beside something I want to pick up?
+    Vector<Treasure> nearby_items = mob.nearbyTreasure(game);
+    for (Treasure treasure: nearby_items)
+    {
+      // Can I carry it?
+      double weight = treasure.getWeight() + mob.getEncumbrance();
+      if (weight > mob.getStrength())
+        continue;
+
+      // Pick it up
+      return mob.pickUp(game, treasure);
+    }
+
+    return false;
+  }
 }
