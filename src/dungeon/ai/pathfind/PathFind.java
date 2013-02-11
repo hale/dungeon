284a24a686772549c63dba8028ddf58f249e6527
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-02-11 11:42:00 +0000

    formatting

--- src/dungeon/ai/pathfind/PathFind.java
+++ src/dungeon/ai/pathfind/PathFind.java
@@ -25,98 +25,98 @@ import dungeon.model.structure.Tile;
  */
 public abstract class PathFind {
 
-	Creature fCreature = null; // the creature that `owns' a corresponding pathfinding instance
-	boolean[][] map = null;
-	boolean[][] tilemap = null;// Array of game tiles, and whether they can be occupied (true), or not (false)
-	boolean mapInitialised=false;     // flag set true after first map calculation
-	int xArraySize = 25;              // limits fixed for the two arrays above
-	int yArraySize = 25;              // by xArraySize and yArraySize
-	double tileSize = 5;              // Tiles are assumed to have a fixed size
-	double halfTileSize = tileSize/2;
+  Creature fCreature = null; // the creature that `owns' a corresponding pathfinding instance
+  boolean[][] map = null;
+  boolean[][] tilemap = null;// Array of game tiles, and whether they can be occupied (true), or not (false)
+  boolean mapInitialised=false;     // flag set true after first map calculation
+  int xArraySize = 25;              // limits fixed for the two arrays above
+  int yArraySize = 25;              // by xArraySize and yArraySize
+  double tileSize = 5;              // Tiles are assumed to have a fixed size
+  double halfTileSize = tileSize/2;
 
-        // constructor for this class, parametrised by the creature that it is for
-	public PathFind(Creature creature) {
-		fCreature = creature;
-	        tilemap = new boolean[xArraySize][yArraySize];
-	        map     = new boolean[xArraySize][yArraySize];
+  // constructor for this class, parametrised by the creature that it is for
+  public PathFind(Creature creature) {
+    fCreature = creature;
+    tilemap = new boolean[xArraySize][yArraySize];
+    map     = new boolean[xArraySize][yArraySize];
 
-	}
+  }
 
-        // Any class that extends this must override findPath
-	abstract public List<Point2D> findPath(Game game, Point2D goal);
+  // Any class that extends this must override findPath
+  abstract public List<Point2D> findPath(Game game, Point2D goal);
 
-        //print the data from both map arrays
-	public void printMap(Game game) {
+  //print the data from both map arrays
+  public void printMap(Game game) {
 
-		System.out.println("map");
-		// populate array by query
-		for (int y = 0; y < yArraySize; y++) {
-			for (int x = 0; x < xArraySize; x++) {
-				int printnum = 0;
-				if (map[x][y])
-					printnum = 1;
-				System.out.print(printnum);
-			}
-			System.out.println("");
-		}
-		System.out.println("\n");
-		System.out.println("tilemap");
-		// populate array by query
-		for (int y = 0; y < yArraySize; y++) {
-			for (int x = 0; x < xArraySize; x++) {
-				int printnum = 0;
-				if (tilemap[x][y])
-					printnum = 1;
-				System.out.print(printnum);
-			}
-			System.out.println("");
-		}
-		System.out.println("\n");
-	}
+    System.out.println("map");
+    // populate array by query
+    for (int y = 0; y < yArraySize; y++) {
+      for (int x = 0; x < xArraySize; x++) {
+        int printnum = 0;
+        if (map[x][y])
+          printnum = 1;
+        System.out.print(printnum);
+      }
+      System.out.println("");
+    }
+    System.out.println("\n");
+    System.out.println("tilemap");
+    // populate array by query
+    for (int y = 0; y < yArraySize; y++) {
+      for (int x = 0; x < xArraySize; x++) {
+        int printnum = 0;
+        if (tilemap[x][y])
+          printnum = 1;
+        System.out.print(printnum);
+      }
+      System.out.println("");
+    }
+    System.out.println("\n");
+  }
 
-        // Figure-out which tiles can be occupied by creature
-        // Populate tilemap with that data
-	public void recalcMap(Game game) {
+  // Figure-out which tiles can be occupied by creature
+  // Populate tilemap with that data
+  public void recalcMap(Game game) {
 
-		if (!mapInitialised) {
-			mapInitialised=true;
+    if (!mapInitialised) {
+      mapInitialised=true;
 
 
-			// System.out.println("map");
-			// populate array by query
-			for (int y = 0; y < yArraySize; y++) {
-				for (int x = 0; x < xArraySize; x++) {
-					Point2D.Double location = new Point2D.Double(halfTileSize
-							+ x * tileSize, halfTileSize + y * tileSize);
-					Tile tile = App.getGame().getMap().getTileAt(location);
-					if (tile != null) {
-						tilemap[x][y] = tile.canOccupy(fCreature);
-						//&& CollisionDetection.canOccupy(game,fCreature,location);
+      // System.out.println("map");
+      // populate array by query
+      for (int y = 0; y < yArraySize; y++) {
+        for (int x = 0; x < xArraySize; x++) {
+          Point2D.Double location = new Point2D.Double(halfTileSize
+              + x * tileSize, halfTileSize + y * tileSize);
+          Tile tile = App.getGame().getMap().getTileAt(location);
+          if (tile != null) {
+            tilemap[x][y] = tile.canOccupy(fCreature);
+            //&& CollisionDetection.canOccupy(game,fCreature,location);
 
-					} else {
-						tilemap[x][y] = false;
-					}
-					// System.out.print(map[x][y]+" ");
-				}
-				// System.out.println("");
-			}
-			// System.out.println("\n");
-			// set own location to true
-			// MappedPoint ownLocation=getMappedPoint(fCreature.getLocation());
-			// map[ownLocation.getX()][ownLocation.getY()] = true;
-		copyarray(); // copy from tilemap into map
-		
-		}
+          } else {
+            tilemap[x][y] = false;
+          }
+          // System.out.print(map[x][y]+" ");
+        }
+        // System.out.println("");
+      }
+      // System.out.println("\n");
+      // set own location to true
+      // MappedPoint ownLocation=getMappedPoint(fCreature.getLocation());
+      // map[ownLocation.getX()][ownLocation.getY()] = true;
+      copyarray(); // copy from tilemap into map
 
-	}
+    }
 
-        // this method copies the tilemap array into the map array
-	public void copyarray(){
+  }
 
-				for (int y = 0; y < yArraySize; y++) {
-				for (int x = 0; x < xArraySize; x++) {
-				map[x][y]=tilemap[x][y];}}
+  // this method copies the tilemap array into the map array
+  public void copyarray(){
 
-	}
+    for (int y = 0; y < yArraySize; y++) {
+      for (int x = 0; x < xArraySize; x++) {
+        map[x][y]=tilemap[x][y];}}
+
+  }
 
 }
