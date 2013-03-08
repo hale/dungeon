69ae4460f0f52e57f1547e1f1acbf42b247f1f33
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-03-08 04:33:55 +0000

    removed unused methods

--- src/dungeon/ai/hale/FactionBehaviour.java
+++ src/dungeon/ai/hale/FactionBehaviour.java
@@ -31,9 +31,7 @@ public class FactionBehaviour implements Behaviour {
   Grid fGrid;
   Faction faction;
   CreatureList fCreatures;
-  Creature fLeader;
   Game fGame;
-  Random fRandom = new Random();
   Point2D fGoal = null;
 
   public FactionBehaviour(Faction faction)
@@ -57,37 +55,14 @@ public class FactionBehaviour implements Behaviour {
 
     this.fCreatures = getFactionCreatures();
 
-    /*
-     * 1. do a path find with:
-     *     originList: all the mobs in this faction
-     *     goalList: all the treasure and enemies
-     * 2. set the creature in the first square of that to be the leader
-     * 3. set the fGoal to be the last square.
-     */
-
     ArrayList<Item> goals = getTreasureAndEnemies();
     ArrayDeque<Point2D> path = fPathFind.findPath(new ArrayList<Item>(fCreatures), goals);
     MapPanel.setPath(new ArrayList<Point2D>(path));
 
     if (path.size() > 1)
-    {
-      //for (Point2D pt : path)
-        //App.log(pt.toString());
-      //fGrid.printSquares(new ArrayList<Point2D>(path), path.getFirst(), path.getLast(), fGame);
-
-      fLeader = closestCreature(path.getLast());
-      // FIXME: the problem is the path contains wrong squares at beginning and/or end.  Print the path
-      assert(fLeader!=null);
-
       fGoal = path.getFirst();
-    }
-      setCreatureGoals();
 
-
-    //if (fGoal == null || goalReached())
-      //fGoal = newGoal();
-
-    //if (fGoal != null)
+    setCreatureGoals();
 
     return true;
   }
@@ -105,48 +80,13 @@ public class FactionBehaviour implements Behaviour {
 
   private void setCreatureGoals()
   {
-    //Creature leader = closestCreatureToGoal();
     CreatureBehaviour behaviour;
     for (Creature creature : fCreatures)
     {
-      //if (creature.getGoal() !=null) continue;
       behaviour = (CreatureBehaviour) creature.getBehaviour();
-      //if (creature.equals(fLeader))
-        behaviour.setDest(fGoal);
-      //else
-        //behaviour.setDest(fLeader.getLocation());
+      behaviour.setDest(fGoal);
     }
   }
-
-  private Creature closestCreature(Point2D pnt)
-  {
-    Creature closest = fCreatures.get(0);
-    double smallest = Double.MAX_VALUE;
-    for (Creature creature : fCreatures)
-    {
-      if (creature.getLocation().distance(closest.getLocation()) < smallest)
-        closest = creature;
-    }
-    return closest;
-
-  }
-
-  //private Creature closestCreatureToGoal()
-  //{
-    //int lowestPathSize = Integer.MAX_VALUE;
-    //assert(!fCreatures.isEmpty());
-    //Creature closestCreature = fCreatures.get(0);
-    //for (Creature creature : fCreatures)
-    //{
-      //int pathSize = (fPathFind.findPath(creature.getLocation(), fGoal)).size();
-      //if (pathSize < lowestPathSize)
-      //{
-        //closestCreature = creature;
-        //lowestPathSize = pathSize;
-      //}
-    //}
-    //return closestCreature;
-  //}
 
   private CreatureList getFactionCreatures()
   {
@@ -167,64 +107,7 @@ public class FactionBehaviour implements Behaviour {
       behaviour.setPathFind(fPathFind);
       creature.setBehaviour(behaviour);
     }
-
   }
-
-  private Point2D newGoal()
-  {
-    Point2D goal_pt = null;
-    if (goal_pt == null)
-      goal_pt = treasureLocation();
-    if (goal_pt  == null)
-      goal_pt = enemyLocation();
-    //if (goal_pt  == null)
-      //goal_pt  = randomLocation();
-    return goal_pt;
-  }
-
-  private Point2D enemyLocation()
-  {
-    for (Creature creature : fGame.getCreatures())
-      if (!fCreatures.contains(creature))
-        return creature.getLocation();
-    return null;
-  }
-
-  private boolean goalReached()
-  {
-    Square goalSquare = fGrid.squareAt(fGoal);
-    for (Creature creature : fCreatures)
-    {
-      Square square = fGrid.squareAt(creature.getLocation());
-      if (square.equals(goalSquare))
-        return true;
-    }
-    return false;
-  }
-
-  private Point2D randomLocation()
-  {
-    Rectangle2D bounds = fGame.getMap().getBounds(0);
-
-    double x = bounds.getX() + (bounds.getWidth() * fRandom.nextDouble());
-    double y = bounds.getY() + (bounds.getHeight() * fRandom.nextDouble());
-    return new Point2D.Double(x, y);
-  }
-
-  private Point2D treasureLocation()
-  {
-    if (fGame.getTreasure().isEmpty()) { return null; }
-    // FIXME: maybe this is bad....
-    Collections.shuffle(fGame.getTreasure());
-    return fGame.getTreasure().get(0).getLocation();
-  }
-
-  private Point2D heroLocation()
-  {
-    if (fGame.getHero() == null) { return null; }
-    return fGame.getHero().getLocation();
-  }
-
 
   @Override
     public boolean deathTick(Game game) {
