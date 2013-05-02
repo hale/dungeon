0de134f937868e10995f920b22ae436d8ab22539
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-05-02 16:47:35 +0100

    added the q table data structure class

---
+++ src/dungeon/ai/hale/QValueStore.java
@@ -0,0 +1,63 @@
+package dungeon.ai.hale;
+
+import java.util.*;
+import java.io.*;
+
+public class QValueStore
+{
+  private HashMap<Object[], Double> store;
+  private static final String FILEPATH = "QValueStore.ser";
+
+  public void saveToDisk()
+  {
+    try{
+      ObjectOutputStream oos = new ObjectOutputStream(
+          new FileOutputStream(FILEPATH));
+      oos.writeObject( store );
+      oos.flush();
+      oos.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void loadFromDisk()
+  {
+    try{
+      ObjectInputStream ois = new ObjectInputStream(
+          new FileInputStream(FILEPATH));
+      store = (HashMap<Object[], Double>) ois.readObject();
+      ois.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  protected double getQValue(State state, Action action)
+  {
+    return store.get(new Object[] { state, action });
+  }
+
+  protected Action getBestAction(State state)
+  {
+    double maxQValue = 0.0;
+    Action bestAction = null;
+    for (Action action : Action.values())
+    {
+      double thisQValue = getQValue( state, action );
+      if (thisQValue > maxQValue)
+      {
+        bestAction = action;
+        maxQValue = thisQValue;
+      }
+    }
+    return bestAction;
+  }
+
+  protected void storeQValue(State state, Action action, double value)
+  {
+    store.put(new Object[] { state, action }, value);
+  }
+
+
+}
