74142727b2721f8e898365175fffc61bc2c65d0a
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-05-03 03:08:11 +0100

    add a random action method

--- src/dungeon/ai/hale/Action.java
+++ src/dungeon/ai/hale/Action.java
@@ -1,6 +1,16 @@
 package dungeon.ai.hale;
 
+import java.util.*;
+
 public enum Action
 {
-  WAIT, MOVE_TO_GOAL
+  WAIT, MOVE_TO_GOAL;
+
+  public static Action random()
+  {
+    if ( new Random().nextInt(2) == 0)
+     return Action.WAIT;
+    else
+      return Action.MOVE_TO_GOAL;
+  }
 }
